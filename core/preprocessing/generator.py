6b7372af5b4d9df509685af5f5f5b4c596e2bf23
fmsunyh <fmsunyh@gmail.com>
2018-12-05 14:45:04 +0800

    pascal data: generator.

---
+++ core/preprocessing/generator.py
@@ -0,0 +1,211 @@
+#!/usr/bin/python3
+
+"""
+Copyright 2018-2019  Firmin.Sun (fmsunyh@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+# -----------------------------------------------------
+# @Time    : 11/8/2018 4:54 PM
+# @Author  : Firmin.Sun (fmsunyh@gmail.com)
+# @Software: ZJ_AI
+# -----------------------------------------------------
+# -*- coding: utf-8 -*-
+import numpy as np
+import random
+import threading
+import warnings
+
+import keras
+from ..utils.image import (
+    resize_image,
+    preprocess_image)
+
+
+class Generator(object):
+    def __init__(
+        self,
+        transform_generator = None,
+        batch_size=1,
+        group_method='random',  # one of 'none', 'random', 'ratio'
+        shuffle_groups=True,
+        image_min_side=600,
+        image_max_side=1000,
+        transform_parameters=None,
+    ):
+        self.transform_generator    = transform_generator
+        self.batch_size             = int(batch_size)
+        self.group_method           = group_method
+        self.shuffle_groups         = shuffle_groups
+        self.image_min_side         = image_min_side
+        self.image_max_side         = image_max_side
+        self.transform_parameters   = transform_parameters
+
+        self.group_index = 0
+        self.lock = threading.Lock()
+
+        self.group_images()
+
+
+    def size(self):
+        raise NotImplementedError('size method not implemented')
+
+    def num_classes(self):
+        raise NotImplementedError('num_classes method not implemented')
+
+    def name_to_label(self, name):
+        raise NotImplementedError('name_to_label method not implemented')
+
+    def label_to_name(self, label):
+        raise NotImplementedError('label_to_name method not implemented')
+
+    def image_aspect_ratio(self, image_index):
+        raise NotImplementedError('image_aspect_ratio method not implemented')
+
+    def load_image(self, image_index):
+        raise NotImplementedError('load_image method not implemented')
+
+    def load_annotations(self, image_index):
+        raise NotImplementedError('load_annotations method not implemented')
+
+    def load_annotations_group(self, group):
+        return [self.load_annotations(image_index) for image_index in group]
+
+    def filter_annotations(self, image_group, annotations_group, group):
+        # test all annotations
+        for index, (image, annotations) in enumerate(zip(image_group, annotations_group)):
+            assert(isinstance(annotations, np.ndarray)), '\'load_annotations\' should return a list of numpy arrays, received: {}'.format(type(annotations))
+
+            # test x2 < x1 | y2 < y1 | x1 < 0 | y1 < 0 | x2 <= 0 | y2 <= 0 | x2 >= image.shape[1] | y2 >= image.shape[0]
+            invalid_indices = np.where(
+                (annotations[:, 2] <= annotations[:, 0]) |
+                (annotations[:, 3] <= annotations[:, 1]) |
+                (annotations[:, 0] < 0) |
+                (annotations[:, 1] < 0) |
+                (annotations[:, 2] > image.shape[1]) |
+                (annotations[:, 3] > image.shape[0])
+            )[0]
+
+            # delete invalid indices
+            if len(invalid_indices):
+                warnings.warn('Image with id {} (shape {}) contains the following invalid boxes: {}.'.format(
+                    group[index],
+                    image.shape,
+                    [annotations[invalid_index, :] for invalid_index in invalid_indices]
+                ))
+                annotations_group[index] = np.delete(annotations, invalid_indices, axis=0)
+
+        return image_group, annotations_group
+
+    def load_image_group(self, group):
+        return [self.load_image(image_index) for image_index in group]
+
+    def random_transform_group_entry(self, image, annotations):
+        # randomly transform both image and annotations
+        if self.transform_generator:
+            pass
+
+        return image, annotations
+
+    def resize_image(self, image):
+        return resize_image(image, min_side=self.image_min_side, max_side=self.image_max_side)
+
+    def preprocess_image(self, image):
+        return preprocess_image(image)
+
+    def preprocess_group_entry(self, image, annotations):
+        # preprocess the image
+        image = self.preprocess_image(image)
+
+        # randomly transform image and annotations
+        image, annotations = self.random_transform_group_entry(image, annotations)
+
+        # resize image
+        image, image_scale = self.resize_image(image)
+
+        # apply resizing to annotations too
+        annotations[:, :4] *= image_scale
+
+        return image, annotations
+
+    def preprocess_group(self, image_group, annotations_group):
+        for index, (image, annotations) in enumerate(zip(image_group, annotations_group)):
+            # preprocess a single group entry
+            image, annotations = self.preprocess_group_entry(image, annotations)
+
+            # copy processed data back to group
+            image_group[index]       = image
+            annotations_group[index] = annotations
+
+        return image_group, annotations_group
+
+    def group_images(self):
+        # determine the order of the images
+        order = list(range(self.size()))
+        if self.group_method == 'random':
+            random.shuffle(order)
+        elif self.group_method == 'ratio':
+            order.sort(key=lambda x: self.image_aspect_ratio(x))
+
+        # divide into groups, one group = one batch
+        self.groups = [[order[x % len(order)] for x in range(i, i + self.batch_size)] for i in range(0, len(order), self.batch_size)]
+
+    def compute_inputs(self, image_group):
+        # get the max image shape
+        max_shape = tuple(max(image.shape[x] for image in image_group) for x in range(3))
+
+        # construct an image batch object
+        image_batch = np.zeros((self.batch_size,) + max_shape, dtype=keras.backend.floatx())
+
+        # copy all images to the upper left part of the image batch object
+        for image_index, image in enumerate(image_group):
+            image_batch[image_index, :image.shape[0], :image.shape[1], :image.shape[2]] = image
+
+        return image_batch
+
+    def compute_targets(self, image_group, annotations_group):
+        targets = np.asarray(annotations_group)
+        return targets
+
+    def compute_input_output(self, group):
+        # load images and annotations
+        image_group       = self.load_image_group(group)
+        annotations_group = self.load_annotations_group(group)
+
+        # check validity of annotations
+        image_group, annotations_group = self.filter_annotations(image_group, annotations_group, group)
+
+        # perform preprocessing steps
+        image_group, annotations_group = self.preprocess_group(image_group, annotations_group)
+
+        # compute network inputs
+        inputs = self.compute_inputs(image_group)
+
+        # compute network targets
+        targets = self.compute_targets(image_group, annotations_group)
+
+        return [inputs, targets], None
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        # advance the group index
+        with self.lock:
+            if self.group_index == 0 and self.shuffle_groups:
+                # shuffle groups at start of epoch
+                random.shuffle(self.groups)
+            group = self.groups[self.group_index]
+            self.group_index = (self.group_index + 1) % len(self.groups)
+
+        return self.compute_input_output(group)
---
+++ core/utils/image.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+"""
+Copyright 2018-2019  Firmin.Sun (fmsunyh@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+# -----------------------------------------------------
+# @Time    : 11/8/2018 4:54 PM
+# @Author  : Firmin.Sun (fmsunyh@gmail.com)
+# @Software: ZJ_AI
+# -----------------------------------------------------
+# -*- coding: utf-8 -*-
+import keras
+import numpy as np
+import cv2
+from PIL import Image
+
+def read_image_bgr(path):
+    try:
+        image = np.asarray(Image.open(path).convert('RGB'))
+    except Exception as ex:
+        print(path)
+
+    return image[:, :, ::-1].copy()
+
+def preprocess_image(x):
+    # mostly identical to "https://github.com/fchollet/keras/blob/master/keras/applications/imagenet_utils.py"
+    # except for converting RGB -> BGR since we assume BGR already
+    x = x.astype(keras.backend.floatx())
+    if keras.backend.image_data_format() == 'channels_first':
+        if x.ndim == 3:
+            x[0, :, :] -= 103.939
+            x[1, :, :] -= 116.779
+            x[2, :, :] -= 123.68
+        else:
+            x[:, 0, :, :] -= 103.939
+            x[:, 1, :, :] -= 116.779
+            x[:, 2, :, :] -= 123.68
+    else:
+        x[..., 0] -= 103.939
+        x[..., 1] -= 116.779
+        x[..., 2] -= 123.68
+
+    return x
+
+def resize_image(img, size=(448, 448)):
+
+    # resize the image with the computed scale
+    img = cv2.resize(img, size, fx=None, fy=None)
+
+    return img
